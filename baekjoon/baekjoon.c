4f0a6e334cb489c7008e56a42ee5ffe9b0d7bfa2
cleats01 <cleats01@gmail.com>
2022-01-05 18:50:22 +0900

    insta_web

---
+++ baekjoon/baekjoon.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int A=1,B=1,X;
+    int c=0;
+    int i=1;
+    scanf("%d",&X);
+    while(1){
+       
+    }
+
+    //X 1 2 4 7 11 16 22
+    //A 1 12 321 1234 54321 123456
+    //B 1 21 123 4321 12345 654321
+
+    printf("%d/%d",A,B);
+}
