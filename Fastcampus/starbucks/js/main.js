1a9e88e4d3be95efc1e4411b45dd6b14d4dc0b58
cleats01 <cleats01@gmail.com>
2022-01-13 14:52:13 +0900

    공통 모듈 분리

--- Fastcampus/starbucks/js/main.js
+++ Fastcampus/starbucks/js/main.js
@@ -1,21 +1,3 @@
-const searchEl = document.querySelector('.search'); //document = html 자체
-const searchInputEl = searchEl.querySelector('input'); //찾아놓은 요소부터 또 찾기
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-})
-
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-})
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -136,6 +118,3 @@ spyEls.forEach(function(spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
